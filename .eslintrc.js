9cc252ae0df99ce83037a38f78fc936b734f5d05
Terence Ponce <terence@mangaroo.moe>
2021-03-29 07:26:45 +0800

    Add ESLint and Prettier for linting

---
+++ .eslintrc.js
@@ -0,0 +1,33 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: [
+    "airbnb",
+    "plugin:react/recommended",
+    "plugin:jsx-a11y/recommended",
+    "plugin:prettier/recommended",
+  ],
+  ignorePatterns: ["node_modules/*", ".next/*", ".out/*", "!.prettierrc.js"],
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 12,
+    sourceType: "module",
+  },
+  plugins: ["react"],
+  rules: {
+    "react/react-in-jsx-scope": "off",
+    "jsx-a11y/anchor-is-valid": [
+      "error",
+      {
+        components: ["Link"],
+        specialLink: ["hrefLeft", "hrefRight"],
+        aspects: ["invalidHref", "preferButton"],
+      },
+    ],
+    "prettier/prettier": ["error", {}, { usePrettierrc: true }],
+  },
+};
